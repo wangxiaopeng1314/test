35fe3e048391eb4f056bd2e615bc36effc61137f
wangxiaopeng1314 <530279459@qq.com>
2020-04-11 10:44:18 +0800

    main

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2020-04-11 10:40
+ * @Created by wxp
+ */
+
+/**
+ * @Auther: wxp
+ * @Date: 2020-04-11 10:40
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
