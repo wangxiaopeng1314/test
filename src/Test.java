8c3bab0e35d57c2b6734c21008ee7b3ad812ee22
wangxiaopeng1314 <530279459@qq.com>
2020-04-11 18:26:09 +0800

    ss

--- src/Test.java
+++ src/Test.java
@@ -14,5 +14,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("nihao");
     }
-    public void fanhui(){}
+    public void fanhui(){
+        System.out.println("nihao");
+    }
 }
